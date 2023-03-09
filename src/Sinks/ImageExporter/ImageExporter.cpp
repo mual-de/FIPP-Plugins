58dd988e441530738d0622deb637c03e38bd1100
mual-de <dev@alexandermaxmueller.de>
2023-03-09 21:04:58 +0100

    implemented image exporter

--- src/Sinks/ImageExporter/ImageExporter.cpp
+++ src/Sinks/ImageExporter/ImageExporter.cpp
@@ -4,26 +4,29 @@ using namespace FIPP;
 using namespace FIPP::plugins;
 #include <FIPP/ImageContainer/ImageFormat.hpp>
 #include <opencv2/opencv.hpp>
+#include <opencv2/cudaimgproc.hpp>
+#include <FIPP/ImageContainer/ImageContainerCUDA.hpp>
+#include <FIPP/ImageContainer/ImageFormat.hpp>
 
 ImageExporter::ImageExporter(YAML::Node config, int elemId, std::shared_ptr<FIPP::logging::ILogger> log) : GenericSinkSi(config["name"].as<std::string>(), elemId, log)
 {
-    if (config["storagePath"])
+    if (config["storagePath"].IsDefined())
     {
-        this->m_storagePath = config["storage_path"].as<std::string>();
+        this->m_storagePath = config["storagePath"].as<std::string>();
     }
     else
     {
-        this->m_storagePath = "~/tmp";
+        this->m_storagePath = "/tmp";
     }
-    if (config["filePrefix"])
+    if (config["filePrefix"].IsDefined())
     {
-        this->m_filePrefix = config["file_prefix"].as<std::string>();
+        this->m_filePrefix = config["filePrefix"].as<std::string>();
     }
     else
     {
         this->m_filePrefix = "img-";
     }
-    if (config["fileFormat"])
+    if (config["fileFormat"].IsDefined())
     {
         this->m_fileFormat = config["fileFormat"].as<std::string>();
     }
@@ -43,6 +46,73 @@ void ImageExporter::initializeInterfaces()
     LOG(logging::CONFIG, "Initialize uneeded interfaces!");
 }
 
+void ImageExporter::exportGpuImages(std::shared_ptr<img::ImageContainer> img)
+{
+
+    if (img->getBackendFlags() == img::BackendFlags::UNIFIED_MEMORY || img->getBackendFlags() == img::BackendFlags::ZERO_COPY)
+    {
+        this->exportCpuImages(img);
+    }
+    else
+    {
+        std::shared_ptr<img::ImageContainerCUDA> imgCuda = std::static_pointer_cast<img::ImageContainerCUDA>(img);
+        img::ImageFormat fmt = img->getImgFormat();
+        FIPP::Point<unsigned int> dims = img->getDims();
+        if (fmt.imgType == img::ImageType::GRAY)
+        {
+            // Grayscale img
+            if (fmt.bitDepthPerPixel == 8)
+            {
+                cv::cuda::GpuMat in(dims.getY(), dims.getX(), CV_8UC1, imgCuda->getDevPtr());
+                cv::cuda::GpuMat out;
+                cv::cuda::cvtColor(in, out, cv::COLOR_GRAY2BGR);
+                cv::Mat exp;
+                out.download(exp);
+                LOG(logging::INFO, "Store image at: " + this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat);
+                cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, exp);
+            }
+            else
+            {
+                // ToDo: 10/12/14/16 Bit Grayscale
+            }
+        }
+        else if (fmt.imgType == img::ImageType::RGB)
+        {
+            // RGB image
+            if (fmt.bitDepthPerPixel == 8)
+            {
+                cv::cuda::GpuMat in(dims.getY(), dims.getX(), CV_8UC3, imgCuda->getDevPtr());
+                cv::cuda::GpuMat out;
+                cv::cuda::cvtColor(in, out, cv::COLOR_RGB2BGR);
+                cv::Mat exp;
+                out.download(exp);
+                LOG(logging::INFO, "Store image at: " + this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat);
+                cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, exp);
+            }
+            else
+            {
+                // ToDo: 10/12/14/16 Bit RGB
+            }
+        }
+        else if (fmt.imgType == img::ImageType::RGBA)
+        {
+            // RGBA img
+            if (fmt.bytesPerPixel == 1)
+            {
+                cv::cuda::GpuMat in(dims.getY(), dims.getX(), CV_8UC4, imgCuda->getDevPtr());
+                cv::cuda::GpuMat out;
+                cv::cuda::cvtColor(in, out, cv::COLOR_RGBA2BGR);
+                cv::Mat exp;
+                cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, exp);
+            }
+            else
+            {
+                // ToDo: 10/12/14/16 Bit RGBA
+            }
+        }
+    }
+}
+
 void ImageExporter::exportCpuImages(std::shared_ptr<img::ImageContainer> img)
 {
     img::ImageFormat fmt = img->getImgFormat();
@@ -50,9 +120,10 @@ void ImageExporter::exportCpuImages(std::shared_ptr<img::ImageContainer> img)
     if (fmt.imgType == img::ImageType::GRAY)
     {
         // Grayscale img
-        if (fmt.bytesPerPixel == 1)
+        if (fmt.bitDepthPerPixel == 8)
         {
             cv::Mat out(dims.getY(), dims.getX(), CV_8UC1, img->getPtr(), 0);
+            LOG(logging::INFO, "Store image at: " + this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat);
             cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, out);
         }
         else
@@ -63,10 +134,13 @@ void ImageExporter::exportCpuImages(std::shared_ptr<img::ImageContainer> img)
     else if (fmt.imgType == img::ImageType::RGB)
     {
         // RGB image
-        if (fmt.bytesPerPixel == 1)
+        if (fmt.bitDepthPerPixel == 8)
         {
             cv::Mat out(dims.getY(), dims.getX(), CV_8UC3, img->getPtr(), 0);
-            cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, out);
+            LOG(logging::INFO, "Store image at: " + this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat);
+            cv::Mat outBGR;
+            cv::cvtColor(out, outBGR, cv::COLOR_RGB2BGR);
+            cv::imwrite(this->m_storagePath + "/" + this->m_filePrefix + std::to_string(this->m_frameNumber) + "." + this->m_fileFormat, outBGR);
         }
         else
         {
@@ -96,5 +170,9 @@ void ImageExporter::doCalculation(std::shared_ptr<img::ImageContainer> img)
     if (type == img::BackendType::CPU)
     {
         this->exportCpuImages(img);
+    }
+    else if (type == img::BackendType::CUDA)
+    {
+        this->exportGpuImages(img);
     }
 }
